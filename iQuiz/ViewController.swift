875d5c141e9451ad7809c69b6216203fe2743c66
OliviaSapp2 <osapp2@uw.edu>
2024-05-01 08:53:15 -0700

    small change

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         // need help look at slides from 4/30 & 5/1
+        // small change
     }
 
 
