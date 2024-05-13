ed54e86c42c29e52c6ec37c14170f03bd024a316
OliviaSapp2 <osapp2@uw.edu>
2024-05-13 08:51:03 -0700

    adding http

---
+++ iQuiz/Pages.swift
@@ -0,0 +1,12 @@
+//
+//  Pages.swift
+//  iQuiz
+//
+//  Created by Olivia Sapp on 5/12/24.
+//
+
+import UIKit
+
+class Pages: NSObject {
+
+}
---
+++ iQuiz/Quiz ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Quiz ViewController.swift
+//  iQuiz
+//
+//  Created by Olivia Sapp on 5/12/24.
+//
+
+import UIKit
+
+class Quiz_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
